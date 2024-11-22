60cfa1818d6c6383e4fc513fef7d8e2835ecabbd
Mahmoud Abuzaid <mahmoud.abuzaid@uniscon.com>
2024-11-22 16:51:11 +0100

    fix path

--- start-appium.sh
+++ start-appium.sh
@@ -13,14 +13,14 @@ NC='\033[0m'       # No Color
 # Default configuration values
 APPIUM_PORT="${APPIUM_PORT:-4723}"           # Default Appium port
 KEEP_ALIVE="${KEEP_ALIVE:-600}"              # Default keep-alive in ms
-HUB_IP="${HUB_IP:-}"                         # Optional hub IP
+HUB_ADDRESS="${HUB_ADDRESS:-}"               # Optional hub address name or ip
 HUB_PORT="${HUB_PORT:-4723}"                 # Default Hub port
-BASE_PATH="${BASE_PATH:-}"                  # Default base path
+BASE_PATH="${BASE_PATH:-}"                   # Default base path
 
 #==============#
 # Start Appium #
 #==============#
-printf "${G}==> ${BL}Starting Appium on port ${YE}${APPIUM_PORT}${G} with keep-alive ${YE}${KEEP_ALIVE}${G} ms and base path ${YE}${BASE_PATH}${G} <==${NC}\n"
+printf "${G}==> ${BL}Starting Appium on port ${YE}${APPIUM_PORT}${G} with keep-alive ${YE}${KEEP_ALIVE}${G} ms <==${NC}\n"
 
 #===============================#
 # Build the base Appium command #
@@ -29,15 +29,22 @@ appium_command="appium server \
     --keep-alive ${KEEP_ALIVE} \
     --use-plugins=device-farm \
     --plugin-device-farm-platform=android \
-    --port ${APPIUM_PORT} \
-    --base-path ${BASE_PATH}"
+    --port ${APPIUM_PORT}"
 
 #=======================================================#
-# Add hub configuration only if HUB_IP is provided #
+# Add hub configuration only if HUB_ADDRESS is provided #
 #=======================================================#
-if [[ -n "${HUB_IP}" ]]; then
-    printf "${G}==> ${BL}Configuring hub address to ${YE}${HUB_IP}:${HUB_PORT}${NC}\n"
-    appium_command+=" --plugin-device-farm-hub=http://${HUB_IP}:${HUB_PORT}"
+if [[ -n "${HUB_ADDRESS}" ]]; then
+    printf "${G}==> ${BL}Configuring hub address to ${YE}${HUB_ADDRESS}:${HUB_PORT}${NC}\n"
+    appium_command+=" --plugin-device-farm-hub=http://${HUB_ADDRESS}:${HUB_PORT}"
+fi
+
+#===========================#
+# Add base path if provided #
+#===========================#
+if [[ -n "${BASE_PATH}" ]]; then
+    printf "${G}==> ${BL}Using base path: ${YE}${BASE_PATH}${NC}\n"
+    appium_command+=" --base-path ${BASE_PATH}"
 fi
 
 #============================#
